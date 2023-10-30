59462817799b297c7f33f832d99f6e12ada6cade
ilotoki0804 <ilotoki0804@gmail.com>
2023-10-30 14:48:52 +0900

    broker 대신 price_cache를 받도록 변경 transactions에 DataFrame을 받을 수 있도록 변경 문서 추가

--- stocks/emulate_trade.py
+++ stocks/emulate_trade.py
@@ -8,20 +8,28 @@ from .dataclasses import SIGNIFICANT_PRICE_NAMES, Transaction, State, INITIAL_ST
 
 
 def emulate_trade(
-    broker: mojito.KoreaInvestment,
-    transactions: list[Transaction],
+    price_cache: PriceCache,
+    transactions: list[Transaction] | pd.DataFrame,
     initial_state: State | None = None,
     only_if_transaction: bool = False,
 ) -> list[State]:
     """거래를 모사해 거래의 결과와 진행 상황을 확인합니다. transactions와 관련한 설명은 Transaction dataclass를 확인하세요.
 
-    주의: 거래는 반드시 시간 순서대로 정렬되어 있어야 합니다.
+    Args:
+        price_cache: PriceCache 인스턴스를 입력으로 받습니다.
+        transactions: transaction들을 입력으로 받습니다. 혹은 그 값을 Dataframe에 돌린 값도 가능합니다.
+            주의: 거래는 반드시 시간 순서대로 정렬되어 있어야 합니다.
+        initial_state: 초기 상태를 정합니다. 이것으로 기존에 가지고 있던 주식이나 예산 등도 정의할 수 있습니다.
+        only_if_transaction: 이 값이 False라면(기본값) transaction이 없는 날도 계산합니다.
+            만약 True라면 Transaction이 있는 날만 계산합니다.
+
+    Returns:
+        State의 list를 반환합니다. Dataframe이 아니라는 점을 주의하세요.
     """
-    price_cache = PriceCache(broker)
     standard_date = datetime(1970, 1, 1)
     initial_state = initial_state or INITIAL_STATE
 
-    transactions_df = pd.DataFrame(transactions)
+    transactions_df = pd.DataFrame(transactions) if isinstance(transactions, list) else transactions
 
     states = [INITIAL_STATE]
     dates: set[datetime] = set(transactions_df["date"].unique())
